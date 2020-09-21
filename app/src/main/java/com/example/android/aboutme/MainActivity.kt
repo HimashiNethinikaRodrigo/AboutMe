5ced4275dd422d62762afe8c6660aee2739a6b63
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-09-21 23:09:11 +0200

    set onclick listener to the done button in order to display type nick name

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.android.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonDone.setOnClickListener {
+            clickHandlerFunction(it)
+        }
+    }
+
+    private fun clickHandlerFunction(view: View) {
+        textViewNickName.text = editTextNickName.text
+        textViewNickName.visibility = View.VISIBLE
+        buttonDone.visibility = View.GONE
+        editTextNickName.visibility = View.GONE
     }
 }
