537ae44d1433e12bcd2be8945acd7bc440150fee
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-09-21 23:10:45 +0200

    auto close keyboard after nick name type and done

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.android.aboutme
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,5 +22,9 @@ class MainActivity : AppCompatActivity() {
         textViewNickName.visibility = View.VISIBLE
         buttonDone.visibility = View.GONE
         editTextNickName.visibility = View.GONE
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
+            view.windowToken,
+            0
+        )
     }
 }
