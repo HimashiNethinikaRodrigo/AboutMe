49c4f6fff99d4165f965ad672534c3bb24360bc5
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-09-22 16:47:06 +0200

    add data binding using ActivityMainBinding

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -5,27 +5,33 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.android.aboutme.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        buttonDone.setOnClickListener {
+        binding.buttonDone.setOnClickListener {
             clickHandlerFunction(it)
         }
 
-        textViewNickName.setOnClickListener {
+        binding.textViewNickName.setOnClickListener {
             updateNickName(it)
         }
     }
 
     private fun clickHandlerFunction(view: View) {
-        textViewNickName.text = editTextNickName.text
-        textViewNickName.visibility = View.VISIBLE
-        buttonDone.visibility = View.GONE
-        editTextNickName.visibility = View.GONE
+        binding.apply {
+            textViewNickName.text = editTextNickName.text
+            textViewNickName.visibility = View.VISIBLE
+            buttonDone.visibility = View.GONE
+            editTextNickName.visibility = View.GONE
+        }
         (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
             view.windowToken,
             0
@@ -33,10 +39,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateNickName(view: View) {
-        buttonDone.visibility = View.VISIBLE
-        editTextNickName.visibility = View.VISIBLE
-        textViewNickName.visibility = View.GONE
-        editTextNickName.requestFocus()
+        binding.apply {
+            buttonDone.visibility = View.VISIBLE
+            editTextNickName.visibility = View.VISIBLE
+            textViewNickName.visibility = View.GONE
+            editTextNickName.requestFocus()
+        }
+
         (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).showSoftInput(
             editTextNickName,
             0
